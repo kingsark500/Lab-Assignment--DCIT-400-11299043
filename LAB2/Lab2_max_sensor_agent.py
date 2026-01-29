fb71e16762bee72f329ef41f8550be804578b92d
kingsark500 <132108589+kingsark500@users.noreply.github.com>
2026-01-29 09:23:38 +0000

    created lab2 code

---
+++ LAB2/Lab2_max_sensor_agent.py
@@ -0,0 +1,37 @@
+from spade.agent import Agent
+from spade.behaviour import PeriodicBehaviour
+import datetime
+import asyncio
+from environment import DisasterEnvironment
+
+class SensorBehaviour(PeriodicBehaviour):
+    async def run (self):
+        percepts = self.env.sense()
+        timestamp = datetime.datetime.now()
+        print(f"[{timestamp}] Sensor readings: {percepts}")
+
+        # Detect critical disaster events
+        for event, value in percepts.items():
+            if value >= 7:
+                print(f"⚠️ ALERT: High {event} detected (severity={value})")
+
+
+class SensorAgent(Agent):
+    async def setup(self):
+        print("SensorAgent started successfully.")
+        self.env = DisasterEnvironment()
+
+        behaviour = SensorBehaviour(period=5)
+        behaviour.env = self.env
+        self.add_behaviour(behaviour)
+
+
+if __name__ == "__main__":
+    agent = SensorAgent(
+        "maxwell_sensor@xmpp.jp"
+        "xdedse200"     
+    )
+
+asyncio.run(agent.start())
+
+
